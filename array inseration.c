3d97ae0e9b779d4979b00ea8d84e22bc7ea87911
kalaiselviv2707 <kalaiselvi.v2025cse@sece.ac.in>
2026-01-19 19:21:05 +0530

    Update array inseration.c

--- array inseration.c
+++ array inseration.c
@@ -1,43 +1,54 @@
-//program to implement list as arrays
-
-
-#include<stdio.h>
+#include <stdio.h>
 #define MAX 50
 
-//Global Declaration
-int arr[MAX],num,pos,loop;
-void delete()
-{
- if(num == 0){
-    printf("array is empty.delete not possible.\n ");
-    return ;
- }
-    printf("Enter position to delete(1 to %d):",num+1);
-    scanf("%d",&pos);
-    if(pos<1||pos>num){
-          printf("Invalid position.\n");
-          return ;
-    }
-     for( int i=pos-1;i<num-1;i++){
-        arr[i]=arr[i+1];
-     }
-     num--;
-     printf("Element deleted successfully.\n");
+// Global Declaration
+int arr[MAX], num, pos, val;
 
-     printf("Array after deleted:\n");
-     for(int i=0;i<num;i++){
-        printf("%d ",arr[i]);
-     }
-     printf("\n");
+void insertElement() {
+    if(num == MAX) {
+        printf("Array is full. Insertion not possible.\n");
+        return;
+    }
+
+    printf("Enter position to insert (1 to %d): ", num+1);
+    scanf("%d", &pos);
+
+    if(pos < 1 || pos > num+1) {
+        printf("Invalid position.\n");
+        return;
+    }
+
+    printf("Enter value to insert: ");
+    scanf("%d", &val);
+
+    // Shift elements to right
+    for(int i = num; i >= pos; i--) {
+        arr[i] = arr[i-1];
+    }
+
+    arr[pos-1] = val; // insert at position
+    num++; // increase size
+
+    printf("Element inserted successfully.\n");
+
+    // Print updated array
+    printf("Array after insertion:\n");
+    for(int i = 0; i < num; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
-int main()
-{
-   printf("Enter the number of to be used\n");
-   scanf("%d",&num);
-   printf("Enter the elements:");
-   for(int i=0;i<num;i++)
-   {
-        scanf("%d",&arr[i]);
-   }
-   delete();
+
+int main() {
+    printf("Enter the number of elements: ");
+    scanf("%d", &num);
+
+    printf("Enter the elements:\n");
+    for(int i = 0; i < num; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    insertElement();
+
+    return 0;
 }
